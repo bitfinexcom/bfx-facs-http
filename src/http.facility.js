152a1c829e609401609437cb22afd5e67e390a97
vigan-abd <vigan.abd@gmail.com>
2025-12-08 18:42:43 +0100

    chore: replace deps

--- src/http.facility.js
+++ src/http.facility.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash')
 const async = require('async')
-const Base = require('bfx-facs-base')
+const Base = require('@bitfinex/bfx-facs-base')
 const fetch = require('node-fetch')
 const HttpError = require('./http.error')
 
